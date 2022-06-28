1420f8caa707268532e2b2e1b91d2c72a475f14a
angrymusic <58168512+angrymusic@users.noreply.github.com>
2022-06-28 13:57:39 +0900

    INIT
    INIT REACT PWA PROJECT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom';
+import Main from './layouts/main';
+import Login from './layouts/login';
 import './App.css';
 
 function App() {
+  const isLogin = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {isLogin ? (
+          <>
+            <Route path="/main" element={<Main />}></Route>
+          </>
+        ):(
+          <>
+            <Route path="/login" element={<Login/>}></Route>
+          </>
+        )}
+      </Routes>
     </div>
   );
 }
---
+++ src/layouts/main.jsx
@@ -0,0 +1,8 @@
+export default function Main(){
+
+    return(
+        <div>
+            Main page
+        </div>
+    )
+}
