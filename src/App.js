9015aa050674c64b186224bac09beb4a091d8129
angrymusic <58168512+angrymusic@users.noreply.github.com>
2022-06-28 17:37:40 +0900

    login-page
    login-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import {Routes, Route} from 'react-router-dom';
-import Main from './layouts/main';
-import Login from './layouts/login';
-import './App.css';
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Main from "./layouts/main";
+import Login from "./layouts/login";
+import "./App.css";
 
 function App() {
-  const isLogin = true;
-  return (
-    <div className="App">
-      <Routes>
-        {isLogin ? (
-          <>
-            <Route path="/main" element={<Main />}></Route>
-          </>
-        ):(
-          <>
-            <Route path="/login" element={<Login/>}></Route>
-          </>
-        )}
-      </Routes>
-    </div>
-  );
+    const isLogin = false;
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    {isLogin ? (
+                        <>
+                            <Route path="*" element={<Main />}></Route>
+                            <Route path="/main" element={<Main />}></Route>
+                        </>
+                    ) : (
+                        <>
+                            <Route path="*" element={<Login />}></Route>
+                            <Route path="/login" element={<Login />}></Route>
+                        </>
+                    )}
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,8 +1,53 @@
+import React, {useState, useEffect} from 'react';
+
+import './style/login.css';
+
 export default function Login(){
+    const [inputId, setInputId]=useState('');
+    const [inputPw, setInputPw]=useState('');
+
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
+ 
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
+ 
+	// login 버튼 클릭 이벤트
+    const onClickLogin = () => {
+        console.log('click login')
+    }
+ 
+	// 페이지 렌더링 후 가장 처음 호출되는 함수
+    // useEffect(() => {
+    //     axios.get('/user_inform/login')
+    //     .then(res => console.log(res))
+    //     .catch()
+    // },
+    // // 페이지 호출 후 처음 한번만 호출될 수 있도록 [] 추가
+    // [])
 
     return(
-        <div>
-            Login page
+        <div className="backGround">
+            <div className='logo'>
+                What To Do Now?
+            </div>
+            <div className="inputBox">
+                <div className="inputBoxContainer">
+                    <div>
+                        <label htmlFor='input_id'>ID : </label>
+                        <input type='text' name='input_id' value={inputId} onChange={handleInputId}/>
+                    </div>
+                    <div>
+                        <label htmlFor='input_pw'>PW : </label>
+                        <input type='text' name='input_pw' value={inputPw} onChange={handleInputPw}/>
+                    </div>
+                    <div>
+                        <button type='button' onClick={onClickLogin}>로그인</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
